b9ff0591c9f4ebf574e0faed8a8c3e6687ee9a5f
Joakim <jmalmberg.web@gmail.com>
2019-07-09 22:50:39 +0200

    Form and routing created

---
+++ rsvplist/src/Form/RSVPForm.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * @file
+ * Contains \Drupal\rsvplist\Form\RSVPForm
+ */
+namespace Drupal\rsvplist\Form;
+
+use Drupal\Core\Database\Database;
+use Drupal\Core\Form\Formbase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Provides an RSVP Email form.
+ */
+class RSVPForm extends FormBase {
+  /**
+   * (@inheritdoc)
+   */
+  public function getFormId() {
+    return 'rsvplist_email_form';
+  }
+
+  /**
+   * (@inheritdoc)
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $node = \Drupal::routeMatch()->getParameter('node');
+    $nid = $node->nid->value;
+    $form['email'] = [
+      '#title'        =>  t('Email Address'),
+      '#type'         =>  'textfield',
+      '#size'         =>  25,
+      '#description'  =>  t("We will send updates to the email address you provide."),
+      '#required'     =>  TRUE,
+    ];
+    $form['submit'] = [
+      '#type'         =>  'submit',
+      '#value'        =>  t('RSVP'),
+    ];
+    $form['nid'] = [
+      '#type'         =>  'hidden',
+      '#value'        =>  $nid,
+    ];
+    return $form;
+  }
+  /**
+   * (@inheritdoc)
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+    $value = $form_state->getValue('email');
+    if ($value == !\Drupal::service('email.validator')->isValid($value)) {
+      $form_state->setErrorByName('email', t('The email address %mail is not valid.', [
+        '%mail' => $value,
+      ]));
+    }
+  }
+
+  /**
+   * (@inheritdoc)
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    drupal_set_message(t('The form is working.'));
+  }
+}
